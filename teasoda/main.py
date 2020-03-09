6701a2441ce2810b9d9399a66aa5c77658970ece
hit-san <soledewa2828@gmail.com>
2020-03-09 18:42:51 +0900

    logging

--- teasoda/main.py
+++ teasoda/main.py
@@ -3,18 +3,26 @@ import time
 import os
 import sys
 import configparser
+import errno
+import logging
+
+home = os.environ['Teasoda_home']
 
 #logger
+def startLogging(logfile_path):
+    logging.basicConfig(filename=logfile_path,level=logging.DEBUG)
 
-#read and set config
-def readConfig():
+def initialize():
+    #read config file
     config = configparser.ConfigParser()
-    config_ini_path = '../config/config.ini'
-    if config_ini_path is None:
-        print("Not found config.ini file")
-        print("Please make config.ini file in config directory")
+    config_ini_path =  home + '/config/config.in'
+    if not os.path.exists(config_ini_path):
+        print("Not found config.ini file. Please make config.ini file in config directory")
         exit(0)
     config.read(config_ini_path, encoding='utf-8')
+    logfile_path = config['logging']['logfile_path']
+    startLogging(logfile_path)
+
 
 #deamonize
 #def daemonize():
@@ -23,4 +31,4 @@ def readConfig():
 
 
 if __name__=='__main__':
-    readConfig()
+    initialize()
